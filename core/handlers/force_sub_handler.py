63b044ea74e338a13028779ca175c91b596c3e89
HB Hammad Dyar <66479406+hbhammaddyar0@users.noreply.github.com>
2021-06-17 10:12:36 +0500

    Update force_sub_handler.py

--- core/handlers/force_sub_handler.py
+++ core/handlers/force_sub_handler.py
@@ -17,7 +17,7 @@ async def handle_force_subscribe(bot, cmd):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/kdramashindi).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
